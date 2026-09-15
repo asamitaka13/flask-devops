35054783e8bc0b454085f2d73d92769659dc29ee
asamitaka13 <lamalasuertepuedesermala@gmail.com>
2026-09-14 20:48:08 -0500

    Actualizando mensaje de bienvenida

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hola, mi nombre es [Tu nombre] y este es mi primer proyecto Devops</h1>"
+    return "<h1>Hola, Este es mi segundo commit, este sistema funciona automaticamente.</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
