b311dc3cad5b33e188d68b540bdb3694a589ec1f
asamitaka13 <lamalasuertepuedesermala@gmail.com>
2026-09-14 22:07:48 -0500

    Añadido gitignore, variables de entorno y pruebas con pytest

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+print("Ruta del archivo .env encontrada:", find_dotenv())
+print("Valor leido en la terminal:", os.getenv("BIENVENIDA_MSG"))
 
 app = Flask(__name__)
 
+MENSAJE_BIENVENIDA = os.getenv("BIENVENIDA_MSG", "Hola Mundo por defecto")
+
 @app.route('/')
 def home():
-    return "<h1>Hola, Este es mi segundo commit, este sistema funciona automaticamente.</h1>"
+    return f"<h1>{MENSAJE_BIENVENIDA}</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
